3bcd7e9bf4bde1ffbb93db5825d1bda18f76926a
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-09-22 23:35:40 +1200

    Added upload route and view

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,4 +5,8 @@ class PagesController < ApplicationController
 
   def index
   end
+
+  def upload
+    @farms = Farm.all
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'farms#index'
   get '/dashboard', to: 'pages#index'
+  get '/upload', to: 'pages#upload'
   resources :farms, only: [:index, :show]
 
   devise_for :users
