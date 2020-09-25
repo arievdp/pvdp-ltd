2f739b2828b9a3b11a14d9049ee5bbfe36b71fb6
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-09-26 09:49:58 +1200

    Fixed bugs in farm controller duplicate records

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -39,9 +39,10 @@ class FarmsController < ApplicationController
   end
 
   def process_csv
-    @farm = Farm.find(farm_params[:farm])
-    @csv = farm_params[:file]
-    raise
+    farm = Farm.find(farm_params[:farm])
+    data = SmarterCSV.process(farm_params[:file])
+    farm_info(data, farm)
+    redirect_to farms_path
   end
 
   private
@@ -79,4 +80,67 @@ class FarmsController < ApplicationController
     @calves.where(birth_date: (1.years.ago..Time.now))
   end
 
+  def farm_info(data, farm)
+    data.each do |d|
+      begin
+        if d[:animal_birth_id_1].present?
+          Animal.create!(
+            farm: farm,
+            birth_id: d[:animal_birth_id_1],
+            cow_number: d[:animal_animal_id],
+            birth_date: DateTime.strptime("20#{d[:animal_birth_id_1].match(/[0-9]{2}/)}", '%Y'),
+            bw_value: d[:calf_bw_2_value],
+            bw_reliability: d[:animal_bw_1_relability],
+            pw_value: d[:animal_pw_value],
+            pw_reliability: d[:animal_pw_reliability],
+            a2_status: d[:animal_a2_status],
+            fate: d[:"animal_custom:_animal_fate_(died)"],
+            expected_calving_date: d[:expected_calving_date],
+            calving_date: d[:animal_calving_date],
+            calf_birth_id: d[:calf_birth_id_2],
+            calf_birth_date: d[:calf_birth_date],
+            calf_sex: d[:calf_sex],
+            calf_fate: d[:calf_fate],
+            dam_birth_id: d[:dam_official_id],
+            dam_cow_number: d[:"dam_cow_no."],
+            sire_birth_id: d[:sire_sire_id],
+            sire_name: d[:sire_name],
+            sex: "F",
+          )
+        end
+      rescue ActiveRecord::RecordInvalid
+        p 'duplicate records'
+      end
+    
+      if d[:calf_birth_id_2].present?
+        begin
+          Animal.create!(
+            farm: farm,
+            birth_id: d[:calf_birth_id_2],
+            # cow_number: d[:animal_animal_id],
+            birth_date: d[:calf_birth_date],
+            bw_value: d[:calf_bw_2_value],
+            bw_reliability: d[:calf_bw_2_reliability],
+            # pw_value: d[:animal_pw_value],
+            # pw_reliability: d[:animal_pw_reliability],
+            # a2_status: d[:animal_a2_status],
+            sex: d[:calf_sex],
+            fate: d[:calf_fate],
+            # expected_calving_date: d[:expected_calving_date],
+            # calving_date: d[:animal_calving_date],
+            # calf_birth_id: d[],
+            # calf_birth_date: d[:calf_birth_date],
+            # calf_sex: d[:calf_sex],
+            # calf_fate: d[:calf_fate],
+            dam_birth_id: d[:animal_birth_id_1],
+            dam_cow_number: d[:animal_animal_id],
+            # sire_birth_id: d[:sire_sire_id],
+            # sire_name: d[:sire_name],
+          )
+        rescue ActiveRecord::RecordInvalid
+          p 'duplicate records'
+        end
+      end
+    end
+  end
 end
