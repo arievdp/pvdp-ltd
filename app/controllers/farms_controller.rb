9eb31c55bd7526dc9c7d8504ed7ed9ea178fea80
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-09-27 15:31:59 +1300

    Finished CSV upload logic

--- app/controllers/farms_controller.rb
+++ app/controllers/farms_controller.rb
@@ -39,30 +39,9 @@ class FarmsController < ApplicationController
   end
 
   def process_csv
-    farm = Farm.find(farm_params[:farm])
-    options = {
-      key_mapping: {
-      animal_birth_id_1: :birth_id,
-      animal_animal_id: :cow_number,
-      animal_bw_1_value: :bw_value,
-      animal_bw_1_reliability: :bw_reliability,
-      animal_pw_value: :pw_value,
-      animal_pw_reliability: :pw_reliability,
-      animal_a2_status: :a2_status,
-      expected_calving_date: :expected_calving_date,
-      animal_calving_date: :calving_date,
-      calf_birth_id_2: :calf_birth_id,
-      calf_birth_date: :calf_birth_date,
-      dam_official_id: :dam_birth_id,
-      sire_sire_id: :sire_birth_id,
-      }
-    }
-    data = SmarterCSV.process(farm_params[:file], options)
-    raise
-
-
-    farm_info(data, farm)
-    redirect_to farm_path(farm)
+    f = Farm.find(farm_params[:farm])
+    Animal.import(farm_params, f)
+    redirect_to farm_path(f)
   end
 
   private
@@ -98,70 +77,6 @@ class FarmsController < ApplicationController
 
   def season
     @calves.where(birth_date: (1.years.ago..Time.now))
-  end
-
-  def farm_info(data, farm)
-    data.each do |d|
-      create_animal(farm, d) if d[:animal_birth_id_1].present?
-      # update_animal(farm, d) if d[:animal_birth_id_1].present? && !unique_birth_id?(d[:animal_birth_id_1])
-      create_calf(farm, d) if d[:calf_birth_id_2].present? && unique_birth_id?(d[:calf_birth_id_2])
-      raise
-    end
-  end
-
-  def create_animal(farm, d)
-    a = Animal.new(
-      farm: farm,
-      birth_id: d[:animal_birth_id_1],
-      cow_number: d[:animal_animal_id],
-      birth_date: DateTime.strptime("20#{d[:animal_birth_id_1].match(/[0-9]{2}/)}", '%Y'),
-      bw_value: d[:calf_bw_2_value],
-      bw_reliability: d[:animal_bw_1_relability],
-      pw_value: d[:animal_pw_value],
-      pw_reliability: d[:animal_pw_reliability],
-      a2_status: d[:animal_a2_status],
-      fate: d[:"animal_custom:_animal_fate_(died)"],
-      expected_calving_date: d[:expected_calving_date],
-      calving_date: d[:animal_calving_date],
-      calf_birth_id: d[:calf_birth_id_2],
-      calf_birth_date: d[:calf_birth_date],
-      calf_sex: d[:calf_sex],
-      calf_fate: d[:calf_fate],
-      dam_birth_id: d[:dam_official_id],
-      dam_cow_number: d[:"dam_cow_no."],
-      sire_birth_id: d[:sire_sire_id],
-      sire_name: d[:sire_name],
-      sex: "F",
-    )
-    if unique_birth_id?(a.birth_id)
-      a.save!
-    else
-      b = Animal.find_by(birth_id: a.birth_id)
-      b = a
-      b.update!
-    end
-  end
-
-  def unique_birth_id?(d)
-    !Animal.where(birth_id: d).exists?
-  end
-
-  def create_calf
-    Animal.create(
-      farm: farm,
-      birth_id: d[:calf_birth_id_2],
-      birth_date: d[:calf_birth_date],
-      bw_value: d[:calf_bw_2_value],
-      bw_reliability: d[:calf_bw_2_reliability],
-      sex: d[:calf_sex],
-      fate: d[:calf_fate],
-      dam_birth_id: d[:animal_birth_id_1],
-      dam_cow_number: d[:animal_animal_id],
-    )
-  end
-
-  def diff_active_record(record_a, record_b)
-    (record_a.attributes.to_a - record_b.attributes.to_a).map(&:first)
   end
 
 end
--- app/models/animal.rb
+++ app/models/animal.rb
@@ -1,7 +1,7 @@
 class Animal < ApplicationRecord
   belongs_to :farm
   validates :birth_id, presence: true, uniqueness: true
-  before_create :heifer?
+  before_create :set_birth_date, :heifer?, :parse_date
   before_save :duplicate?
 
   def calved?
@@ -12,7 +12,88 @@ class Animal < ApplicationRecord
     fate != 'Died'
   end
 
+  def self.import(params, f)
+    SmarterCSV.process(params[:file], Animal.csv_options) do |array|
+      a = array.first
+      create_heifer(a, f) if a[:birth_id].present? && !Animal.duplicate?(a)
+      create_calf(a, f) if a[:calf_birth_id].present? && !Animal.duplicate?(a)
+    end
+  end
+
   private
+
+  def self.csv_options
+    # Use these keys to change the defauly naming of the CSV columns
+    {
+      key_mapping: 
+      {
+        animal_birth_id_1: :birth_id,
+        animal_animal_id: :cow_number,
+        animal_bw_1_value: :bw_value,
+        animal_bw_1_reliability: :bw_reliability,
+        animal_pw_value: :pw_value,
+        animal_pw_reliability: :pw_reliability,
+        animal_a2_status: :a2_status,
+        expected_calving_date: :expected_calving_date,
+        animal_calving_date: :calving_date,
+        calf_birth_id_2: :calf_birth_id,
+        calf_birth_date: :calf_birth_date,
+        dam_official_id: :dam_birth_id,
+        "dam_cow_no.": :dam_cow_number,
+        sire_sire_id: :sire_birth_id,
+        "animal_custom:_animal_fate_(died)": :fate,
+        calf_bw_2_value: :calf_bw,
+        calf_bw_2_reliability: :calf_bw_reliability,
+      }
+    }
+  end
+
+  def self.duplicate?(a)
+    Animal.where(birth_id: a[:birth_id]).exists?
+  end
+
+  def self.create_heifer(a, f)
+    Animal.create(
+      farm: f,
+      birth_id: a[:birth_id],
+      cow_number: a[:cow_number],
+      birth_date: a[:birth_date],
+      bw_value: a[:bw_value],
+      bw_reliability: a[:bw_reliability],
+      pw_value: a[:pw_value],
+      pw_reliability: a[:pw_reliability],
+      a2_status: a[:a2_status],
+      fate: a[:fate],
+      expected_calving_date: a[:expected_calving_date],
+      calving_date: a[:calving_date],
+      calf_birth_id: a[:calf_birth_id],
+      calf_birth_date: a[:calf_birth_date],
+      calf_sex: a[:calf_sex],
+      calf_fate: a[:calf_fate],
+      dam_birth_id: a[:dam_birth_id],
+      dam_cow_number: a[:dam_cow_number],
+      sire_birth_id: a[:sire_birth_id],
+      sire_name: a[:sire_name],
+    )
+  end
+
+  def self.create_calf(a, f)
+    heifer = Animal.create(
+      farm: f,
+      birth_id: a[:calf_birth_id],
+      birth_date: a[:calf_birth_date],
+      bw_value: a[:calf_bw],
+      bw_reliability: a[:calf_bw_reliability],
+      sex: a[:calf_sex],
+      fate: a[:calf_fate],
+      dam_birth_id: a[:birth_id],
+      dam_cow_number: a[:cow_number],
+    )
+  end
+
+  def set_birth_date
+    self[:birth_date] = DateTime.strptime("20#{self[:birth_id].match(/[0-9]{2}/)}", '%Y')
+  end
 
   def heifer?
     current_year = Time.current.year
@@ -21,11 +102,15 @@ class Animal < ApplicationRecord
       a[:status] = 'Calf'
     elsif a[:birth_date].to_date.year == (current_year - 1)
       a[:status] = 'Yearling'
+      a[:sex] = "F"
     else
       a[:status] = 'Heifer'
+      a[:sex] = "F"
     end
   end
 
-  def duplicate?
+  def parse_date
+    self[:calving_date] = Date.parse(self[:calving_date])
   end
+
 end
