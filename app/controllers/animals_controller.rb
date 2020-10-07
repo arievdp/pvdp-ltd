f0302895ef8a8edb3cb7c05a238ca569c323d967
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-10-07 18:32:18 +1300

    Finished logic for animal update

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,6 +1,6 @@
 class AnimalsController < ApplicationController
-    before_action :set_farm, only: [:index]
-    before_action :set_animal, only: [:edit]
+    before_action :set_farm, only: [:index, :update]
+    before_action :set_animal, only: [:edit, :update]
 
     def index
         @animals = @farm.animals
@@ -8,6 +8,15 @@ class AnimalsController < ApplicationController
 
     def edit
         
+    end
+
+    def update
+        @animal.update(animal_params)
+        if Animal.duplicates?.exists?
+            redirect_to '/duplicates'
+        else
+            redirect_to farm_path(@farm)
+        end
     end
 
     def duplicates
@@ -21,7 +30,32 @@ class AnimalsController < ApplicationController
     end
 
     def set_farm
-        @farm = Farm.find(params[:farm_id])
+        @farm = Farm.find(animal_params[:farm_id])
+    end
+
+    def animal_params
+        params.require(:animal).permit(
+            :id,
+            :farm_id,
+            :birth_id, 
+            :cow_number, 
+            :bw_value, 
+            :bw_reliability, 
+            :pw_value, 
+            :pw_reliability, 
+            :a2_status, 
+            :fate, 
+            :expected_calving_date,
+            :calving_date,
+            :calf_birth_id, 
+            :calf_birth_date,
+            :calf_sex, 
+            :calf_fate, 
+            :dam_birth_id, 
+            :dam_cow_number, 
+            :sire_birth_id, 
+            :sire_name, 
+        )
     end
 
 end
