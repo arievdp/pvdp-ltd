df87e6845bbb8083025efbcb5deec6fd36c2aab3
Arie <arie.theodorus.vanderpoel@gmail.com>
2020-10-07 19:27:14 +1300

    Added logic and helper method for sortable columns

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+    # url: https://www.youtube.com/watch?v=cWYiAVMSHD4&ab_channel=Developer%27sBox
+    def sortable(column, title = nil)
+        title ||= column.titleize
+        # css_class = column == sort_column ? "current #{sort_directions}" : nil
+        direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+        link_to title, sort: column, direction: direction
+    end 
 end
